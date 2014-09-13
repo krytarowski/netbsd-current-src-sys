662163f9853982683fdf9e09918170404f0a168e
Kamil Rytarowski <n54@gmx.com>
2014-09-14 00:00:24 +0200

    advfs: <kern/sched_prim.h> and <fs/msfs/advfs_evm.h> are missing

--- fs/msfs/bs_bitfile_sets.c
+++ fs/msfs/bs_bitfile_sets.c
@@ -51,9 +51,13 @@
 #endif
 #include <fs/msfs/bs_stripe.h>
 #include <fs/msfs/bs_extents.h>
+#if 0 /* XXX: missing */
 #include <kern/sched_prim.h>
+#endif
 #include <kern/event.h>
+#if 0 /* XXX: missing */
 #include <fs/msfs/advfs_evm.h>
+#endif
 #include <fs/msfs/bs_msg_queue.h>
 
 #include <sys/stat.h>
