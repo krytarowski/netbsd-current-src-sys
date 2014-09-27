38a229fc16aa66ba18455347426999af69d18a39
Kamil Rytarowski <n54@gmx.com>
2014-09-27 21:30:27 +0200

    BS: Adapt includes of bs_extents.c for NetBSD

--- external/gpl2/msfs/dist/bs/bs_extents.c
+++ external/gpl2/msfs/dist/bs/bs_extents.c
@@ -44,7 +44,9 @@
 #pragma ident "@(#)$RCSfile: bs_extents.c,v $ $Revision: 1.1.145.7 $ (DEC) $Date: 2006/01/03 18:54:59 $"
 #endif
 
-#include <sys/lock_probe.h>
+#include <sys/param.h>
+#include <sys/types.h>
+
 #include "../msfs/ms_public.h"
 #include "../msfs/ms_privates.h"
 #include "../msfs/bs_bmt.h"
