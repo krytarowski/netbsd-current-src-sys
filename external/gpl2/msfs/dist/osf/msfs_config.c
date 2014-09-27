4663a96205ee63d0d63573f10a40665a5c8c3372
Kamil Rytarowski <n54@gmx.com>
2014-09-28 01:17:58 +0200

    OSF: Adapt includes of msfs_config.c for NetBSD

--- external/gpl2/msfs/dist/osf/msfs_config.c
+++ external/gpl2/msfs/dist/osf/msfs_config.c
@@ -41,7 +41,8 @@
 #pragma ident "@(#)$RCSfile: msfs_config.c,v $ $Revision: 1.1.10.3 $ (DEC) $Date: 1997/06/18 15:35:22 $"
 #endif
 
-#include <sys/sysconfig.h>
+#include <sys/param.h>
+#include <sys/types.h>
 #include <sys/mount.h>
 #include <sys/errno.h>
 
