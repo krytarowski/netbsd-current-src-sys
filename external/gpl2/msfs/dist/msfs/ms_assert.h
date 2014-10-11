370308bc0365290d2c778f847d8d26eb506b14ae
Kamil Rytarowski <n54@gmx.com>
2014-10-11 14:23:50 +0200

    MSFS: Design new debug levels and functions
    Replace the original:
    - MS_SMP_ASSERT(),
    - MS_DBG_ASSERT(),
    - MS_MACRO_ASSERT(),
    - ADVFS_NO_ZERO_ALLOCATE,
    - MS_DEBUG_NOCOREDUMP
    
    with a new interface:
    - msfs_assert(),
    - msfs_debug_assert(),
    - msfs_extensive_tests().
    
    Rename AdvfsEnableAsserts to MsfsDebugLevel for sysctl(8) levels.
    
    Introduce new defines:
    - MSFS_DEBUG (in place of ADVFS_DEBUG) and
    - MSFSDEBUGLEVEL_DEFAULT.

--- external/gpl2/msfs/dist/msfs/ms_assert.h
+++ external/gpl2/msfs/dist/msfs/ms_assert.h
@@ -27,98 +27,94 @@
 #define MS_ASSERT_H
 
 /*
- * There are 2 MS_XXX_ASSERT() macros:
- *   1. MS_SMP_ASSERT()  - the normal assert; can be turned on in any version
- *                         of the kernel by #defining ADVFS_SMP_ASSERT.
- *   2. MS_DBG_ASSERT()  - a version that exists whenever ADVFS_DEBUG is
- *                         defined.  Note that you can force this assertion
- *                         to skip the coredump and just print an error
- *                         message by setting -DMS_DEBUG_NOCOREDUMP in the
- *                         makefile before compiling.
+ * Redesigned debug functionality from scratch and adapted for NetBSD.
+ * There is no code-share with the original Tru64 work, but keep the original
+ * copy-right notice for credit.
  *
- *  To remove ASSERTs for product ship, just comment out the #define
- *  of ADVFS_SMP_ASSERT in the next section, and be sure no one mistakenly
- *  #defines ADVFS_DEBUG in the makefile.
- *
- *  To disable ASSERTs for a kernel that has them compiled in, use
- *  sysconfig or sysconfigdb or dbx to set AdvfsEnableAsserts to 0.
+ * To enable debugging define MSFS_DEBUG in compilation-time (eg -DMSFS_DEBUG).
  */
+#ifdef MSFS_DEBUG
 
-#define ADVFS_SMP_ASSERT
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/systm.h>
+#include <sys/stdarg.h>
 
 /*
- * Next section determines the declaration for MS_SMP_ASSERT()
+ * Debug levels:
+ *   0 - disabled
+ *   1 - enable standard asserts (fatal)
+ *   2 - enable debug asserts (non fatal)
+ *  >2 - enable extensive tests
+ *
+ * Change debug levels in run-time with sysctl(8).
+ *
+ * The default debug level is possible to specify in compilation time,
+ * e.g.: -DMSFSDEBUGLEVEL_DEFAULT=0
+ *
+ * If the default level is not explicitly specified then it defaults to 1.
+ *
+ * WARNING: Specifing MsfsDebugLevel to 2 in run-time enables additional
+ *          code paths. It may be dangerous to hot-swap it.
  */
-#ifdef ADVFS_SMP_ASSERT 
- 
-    extern unsigned int AdvfsEnableAsserts;
+extern unsigned int MsfsDebugLevel;
 
-#   define MS_SMP_ASSERT( ex )                   \
-        if (AdvfsEnableAsserts && !(ex)) {       \
-            ADVFS_SAD0("SMP Assertion failed");  \
-        }
-
-    int
-    macro_sad( char *module,
-               int   line,
-               char *fmt,
-               char *msg,
-               long  n1,
-               long  n2,
-               long  n3);
-
-    /*
-     * MS_MACRO_ASSERT - Returns TRUE if AdvfsEnableAsserts is disabled, Returns
-     * TRUE if AdvfsEnableAsserts is enabled _AND_ (ex) is TRUE, else
-     * MS_MACRO_ASSERT will panic the system.  This MACRO is intended to be used
-     * within other MACROs (such as BFSET_VALID, DMN_HTOP, and VD_HTOP).
-     *
-     * If ADVFS_SMP_ASSERT is not defined (it is only defined during development
-     * cycles), then MS_MACRO_ASSERT always returns TRUE.
-     */
-         
-#   define MS_MACRO_ASSERT(ex)                                                        \
-        ( AdvfsEnableAsserts && !(ex) ?                                               \
-          macro_sad( __FILE__, __LINE__, SadFmt0, "SMP Assertion failed", 0, 0, 0 ) : \
-          TRUE )
-
-#else   /* ADVFS_SMP_ASSERT */
- 
-#   define MS_MACRO_ASSERT(ex)  TRUE
-#   define MS_SMP_ASSERT( ex )
-
-#endif  /* ADVFS_SMP_ASSERT */
- 
-
-/*
- * Next section determines the declaration for MS_DBG_ASSERT()
+/**
+ * Assert an expression and print a debug message in case of a false statement.
+ * This assert is fatal and will panic the running system.
+ *
+ * @param ex
+ *   Evaluation of an expression
+ * @param fmt
+ *   Debug message to be displayed in case of triggering a false assert
  */
-#ifdef ADVFS_DEBUG
-#   ifdef MS_DEBUG_NOCOREDUMP
-#       define MS_DBG_ASSERT( ex )                                             \
-            if (!(ex)) {                                                       \
-                printf( "Assertion failed: file: \"%s\", line: %d test: %s\n", \
-                __FILE__, __LINE__, "ex");                                     \
-                enter_kdebug();                                                \
-            }
-                
-#   else  /* MS_DEBUG_NOCOREDUMP */
-#       define MS_DBG_ASSERT( ex )                 \
-            if (!(ex)) {                           \
-                ADVFS_SAD0("Assertion failed");    \
-            }
-#   endif /* MS_DEBUG_NOCOREDUMP */
+static inline void msfs_assert(int ex, char *fmt, ...)
+{
+    if (MsfsDebugLevel && !ex) {
+        va_list ap;
+        va_start(ap, fmt);
+        vpanic(fmt, ap);
+        va_end(ap);
+    }
+}
 
-#else  /* ADVFS_DEBUG */
-
-#   define MS_DBG_ASSERT( ex )
-
-#endif /* ADVFS_DEBUG */
-
-
-/* DO NOT SUBMIT - uncomment ADVFS_NO_ZERO_ALLOCATE
- * define only in developer builds for test kernels.
- * #define ADVFS_NO_ZERO_ALLOCATE
+/**
+ * Assert an expression and print a debug message in case of a false statement.
+ * This assert is not fatal and will not panic the running system.
+ *
+ * @param ex
+ *   Evaluation of an expression
+ * @param fmt
+ *   Debug message to be displayed in case of triggering a false assert
  */
+static inline void msfs_debug_assert(int ex, char *fmt, ...)
+{
+    if (MsfsDebugLevel > 1 && !ex) {
+        va_list ap;
+        va_start(ap, fmt);
+        vprintf(fmt, ap);
+        va_end(ap);
+    }
+}
+
+/**
+ * Check the current debug level for demand of additional extensive tests.
+ *
+ * @return
+ *   - 1 if the extensive tests are desired to be enabled
+ *   - 0 if the extensive tests are not desired
+ */
+static inline int msfs_extensive_tests(void)
+{
+	return (MsfsDebugLevel > 2);
+}
+
+#else  /* MSFS_DEBUG */
+
+#define msfs_assert()           do { } while(0)
+#define msfs_debug_assert()     do { } while(0)
+#define msfs_extensive_tests()  (0)
+
+#endif /* MSFS_DEBUG */
 
 #endif /* MS_ASSERT_H */
