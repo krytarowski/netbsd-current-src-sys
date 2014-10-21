a55394855156d23b5c7eb641845b2f8e3abe1e86
Kamil Rytarowski <n54@gmx.com>
2014-10-21 22:49:44 +0200

    BS: Change DELLIST_LOCK_WRITE and UNLOCK to inline functions

--- external/gpl2/msfs/dist/bs/bs_delete.c
+++ external/gpl2/msfs/dist/bs/bs_delete.c
@@ -84,11 +84,14 @@
  */
 #define MCIDEQL(i1, i2) (((i1).page == (i2).page) && ((i1).cell == (i2).cell))
 
-#define  DELLIST_LOCK_WRITE( lk ) { \
-    lock_write( lk.lock );\
+static inline void DELLIST_LOCK_WRITE(krwlock_t *lk)
+{
+    lock_write( lk.lock );
 }
-#define  DELLIST_UNLOCK( lk ) { \
-    rw_exit( lk.lock );\
+
+static inline void DELLIST_UNLOCK(krwlock_t *lk)
+{
+    rw_exit( lk.lock );
 }
 
 /* Internal Types */
